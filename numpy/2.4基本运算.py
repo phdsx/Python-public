6c77d5967efab7e2a408212bdda2b24158530dac
phdsx <pp93314@163.com>
2020-04-20 23:57:44 +0800

    补充提交

---
+++ numpy/2.4基本运算.py
@@ -0,0 +1,12 @@
+# -*- encoding = utf-8 -*-
+#   作者   ： phdsx
+# 创建时间  ： 2020/4/20 23:24
+# 文件名称  ： 2.4基本运算.py
+# 所属工程  ： Python-learning
+# 版本号    ： Ver.
+
+import numpy as np
+
+a=np.arange(9).reshape((3,3))
+print(a)
+print(a.T)
