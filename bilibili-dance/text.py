34572d5c1dfa84b88d7c7748fda89213ddfb5d57
phdsx <unnamed>
2020-04-29 16:48:48 +0800

    api更新

--- bilibili-dance/text.py
+++ bilibili-dance/text.py
@@ -6,10 +6,14 @@
 # 所属工程  ：  Python-learning
 # 版本号    ：  Ver.
 
-import requests
+import requests,json
 from lxml import etree
 
-r=requests.get(r'https://www.bilibili.com/v/dance/otaku/#/all/default/0/1')
-html=etree.HTML(r.text)
-print(r.text)
-print(html.xpath(r'//*[@id="videolist_box"]/div[2]/ul/li[9]/div/div[2]/a'),1)
+i=1
+headers={
+            "user-agent":"Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36"
+        }
+r=requests.get(rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20',headers=headers)
+print(r.status_code)
+data=json.loads(r.text)
+print(data.get('data').get("archives"))
