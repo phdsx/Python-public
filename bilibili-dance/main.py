0534aecf2008bc44dab6bfd9b80da11e138568b4
phdsx <pp93314@163.com>
2020-04-30 00:29:05 +0800

    b站

--- bilibili-dance/main.py
+++ bilibili-dance/main.py
@@ -16,14 +16,12 @@ class Bilibili():
         }
 
     #获取网页内容
-    def get_html(self,url,xpath):
+    def get_newlist(self,url):
         error_time=0
         while error_time<3:
             r = requests.get(url, headers=self.headers,timeout=3)
             if r.status_code==200:
-                r.encoding='utf-8'
-                html=etree.HTML(r.text)
-                return html.xpath(xpath)
+                return r.text
             else:
                 error_time+=1
                 time.sleep(3)
@@ -33,7 +31,7 @@ class Bilibili():
         # i为页码，为方便测试暂定为1
         i = 1
         # 按时间排序视频页，获取网址列表
-        url = fr'https://www.bilibili.com/v/dance/otaku/#/all/default/0/{i}'
+        url = rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20'
         error_time=0
         r = requests.get(url, headers=self.headers)
         print(r.status_code)
@@ -42,7 +40,7 @@ class Bilibili():
     #获取视频信息，以字典形式｛BV号：｛‘点赞’：？，“评论”：？，‘收藏’：？，‘弹幕’：？，‘播放量’：？，“投稿时间”：？｝｝
     def get_video_info(self):
         pass
-
+i=1
 b=Bilibili()
-a=b.get_html(r'https://www.bilibili.com/v/dance/otaku/#/all/default/0/1',r'//*[@id="videolist_box"]/div[2]/ul/li/div/div[2]/a/@href')
+a=b.get_newlist(rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20')
 print(a)
