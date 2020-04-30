df1bdeb0da8878c9586fa83dba057068961272ea
phdsx <unnamed>
2020-04-30 14:46:32 +0800

    b站

--- bilibili-dance/main.py
+++ bilibili-dance/main.py
@@ -6,7 +6,7 @@
 # 所属工程  ：  Python-learning
 # 版本号    ：  Ver.
 
-import requests, time
+import requests, time,json
 from lxml import etree
 
 
@@ -15,6 +15,7 @@ class Bilibili():
         self.headers = {
             "user-agent": "Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36"
         }
+        self.video_data={}
 
     # 获取网页内容
     def get_newlist(self, url):
@@ -22,14 +23,32 @@ class Bilibili():
         while error_time < 3:
             r = requests.get(url, headers=self.headers, timeout=3)
             if r.status_code == 200:
-                return r.text
+                data=json.loads(r.text)
+                return data
             else:
                 error_time += 1
                 time.sleep(3)
-        return '错误'
+        return None
 
+    def get_data(self,url):
+        data = self.get_newlist(url).get('data').get("archives")
+        return data
+
+    def get_video_info(self,url):
+        datas=self.get_data(url)
+        for each in datas:
+            self.video_data['aid']=each.get('aid')
+            self.video_data['tname'] = each.get('tname')
+            self.video_data['copyright'] = each.get('copyright')
+            self.video_data['title'] = each.get('title')
+            self.video_data['pubdate'] = each.get('pubdate')
+            self.video_data['ctime'] = each.get('ctime')
+            self.video_data['owner'] = [each.get('owner').get('mid'),each.get('owner').get('name')]
+            self.video_data['stat'] = each.get('stat')
+
+        return self.video_data
 
 i = 1
 b = Bilibili()
-a = b.get_newlist(rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20')
+a = b.get_video_info(rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20')
 print(a)
--- bilibili-dance/text.py
+++ bilibili-dance/text.py
@@ -9,11 +9,6 @@
 import requests,json
 from lxml import etree
 
-i=1
-headers={
-            "user-agent":"Mozilla/5.0(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36"
-        }
-r=requests.get(rf'https://api.bilibili.com/x/web-interface/newlist?rid=20&type=0&pn={i}&ps=20',headers=headers)
-print(r.status_code)
-data=json.loads(r.text)
-print(data.get('data').get("archives"))
+a={'a':122,"b":{'d':233}}
+
+print(a.get('b').get('d'))
