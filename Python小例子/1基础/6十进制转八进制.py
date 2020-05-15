9bb182b1f2fb39a9a9687b8a079a73022ce53b1f
phdsx <unnamed>
2020-05-15 11:30:45 +0800

    小例子

---
+++ Python小例子/1基础/6十进制转八进制.py
@@ -0,0 +1,10 @@
+# -*- encoding : UTF-8 -*-
+# Author   ：  phdsx
+# 创建时间  ：  2020/5/15 11:17
+# 编译器    ：  PyCharm
+# 文件名    ：  6十进制转八进制.py
+# 所属工程  ：  Python-learning
+# 版本号    ：  Ver.
+
+a=4324
+print(oct(a))
