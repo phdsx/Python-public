e05e81167bca000a64429af62343591719d94bc0
phdsx <unnamed>
2020-05-15 11:03:46 +0800

    小例子

---
+++ Python小例子/1基础/3元素至少一个为真.py
@@ -0,0 +1,10 @@
+# -*- encoding : UTF-8 -*-
+# Author   ：  phdsx
+# 创建时间  ：  2020/5/15 10:49
+# 编译器    ：  PyCharm
+# 文件名    ：  3元素至少一个为真.py
+# 所属工程  ：  Python-learning
+# 版本号    ：  Ver.
+
+a=[1233,4352,45353,2344,'']
+print(any(a))
---
+++ Python小例子/1基础/返回字符串.py
@@ -0,0 +1,7 @@
+# -*- encoding : UTF-8 -*-
+# Author   ：  phdsx
+# 创建时间  ：  2020/5/15 11:02
+# 编译器    ：  PyCharm
+# 文件名    ：  返回字符串.py
+# 所属工程  ：  Python-learning
+# 版本号    ：  Ver.
